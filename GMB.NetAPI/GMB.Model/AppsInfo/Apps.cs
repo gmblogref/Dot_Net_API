ef9854f213010d40cce415a0cc588187cec4332d
Geoffrey Blogref <gmblogref@hotmail.com>
2020-05-12 10:48:14 -0500

    adding Models DLL to the API project

---
+++ GMB.NetAPI/GMB.Model/AppsInfo/Apps.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMB.Model.AppsInfo
+{
+    public class Apps
+    {
+        public int AppsId { get; set; }
+        public string AppName { get; set; }
+        public string AppDesc { get; set; }
+    }
+}
