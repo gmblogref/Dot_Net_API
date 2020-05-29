4bfb029ece7dd227e862a480ddd59a5f28fdd1ac
Geoffrey Blogref <gmblogref@hotmail.com>
2020-05-29 12:16:41 -0500

    Adding business logic layer
    update delete to be Task void

---
+++ GMB.NetAPI/GMB.BusinessLogic/AppsLogic/AppsLogic.cs
@@ -0,0 +1,40 @@
+﻿using GMB.Model.AppsInfo;
+using GMB.Repository;
+using GMB.Repository.AppsRepo;
+using GMB.Repository.UserRepo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMB.BusinessLogic.AppsLogic
+{
+    /// <summary>
+    /// Apps business logic
+    /// </summary>
+    public class AppsLogic : IAppsLogic
+    {
+        AppsRepository repo;
+        public AppsLogic(GMBDataConnection connection)
+        {
+            this.repo = connection.GetAs<AppsRepository>();
+        }
+
+        public async Task<IEnumerable<Apps>> GetAll()
+        {
+            var appList = await repo.GetAllApplications();
+            return appList;
+        }
+
+        public async Task<int> Insert(Apps app)
+        {
+            return await repo.InsertApplication(app);
+        }
+
+        public async Task Update(Apps app)
+        {
+            await repo.UpdateApplication(app);
+        }
+    }
+}
---
+++ GMB.NetAPI/GMB.BusinessLogic/UserAccountsLogic/UserAccountsLogic.cs
@@ -0,0 +1,49 @@
+﻿using GMB.Model.UserInfo;
+using GMB.Repository;
+using GMB.Repository.UserRepo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMB.BusinessLogic.UserAccountsLogic
+{
+    /// <summary>
+    /// User Accounts business logic
+    /// </summary>
+    public class UserAccountsLogic : IUserAccountsLogic
+    {
+        UserAccountsRepository repo;
+        public UserAccountsLogic(GMBDataConnection connection)
+        {
+            this.repo = connection.GetAs<UserAccountsRepository>();
+        }
+
+        public async Task Delete(int userId)
+        {
+            await repo.DeleteUserAccount(userId);
+        }
+
+        public async Task<IEnumerable<UserAccounts>> GetAll()
+        {
+            var userList = await repo.GetAllUserAccounts();
+            return userList;
+        }
+
+        public async Task<UserAccounts> GetById(int userId)
+        {
+            return await repo.GetByIdUserAccount(userId);
+        }
+
+        public async Task<int> Insert(UserAccounts user)
+        {
+            return await repo.InsertUserAccount(user);
+        }
+
+        public async Task Update(UserAccounts user)
+        {
+            await repo.UpdateUserAccount(user);
+        }
+    }
+}
